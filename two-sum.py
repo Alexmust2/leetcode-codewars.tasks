a9ed6a075c944d0f53cd1e90593d17e7bfaaa917
Alexmust2 <alex.must2003@mail.ru>
2022-01-15 23:02:36 +0300

    resolving two-sum task

---
+++ two-sum.py
@@ -0,0 +1,12 @@
+from typing import *
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       for i in range(len(nums)):
+           for j in range(len(nums)):
+               if i != j and nums[i] + nums[j] == target:
+                   return [i,j]
+
+
+s = Solution()
+print(s.twoSum([2,7,11,15], 9))
+
