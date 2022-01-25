d2b7108f069a8cc3963b19208ab02fa842923ec1
Alexmust2 <alex.must2003@mail.ru>
2022-01-25 19:57:50 +0300

    codewars task

---
+++ Vowel remover.py
@@ -0,0 +1,22 @@
+from operator import le
+from os import remove
+from re import U
+
+
+def shortcut(s):
+    s = list(s)
+    while "o" in s:
+        s.remove("o")
+    while "e" in s:
+        s.remove("e") 
+    while "a" in s:
+        s.remove("a")
+    while "i" in s:
+        s.remove("i")
+    while "u" in s:
+        s.remove("u")
+    return "".join(s)
+
+
+
+print(shortcut('hello'))
